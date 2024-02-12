c4104d2d139eadc39e4e487907ca85cfa1dc634a
fadikbb <fadikbb2004@gmail.com>
2024-02-12 19:54:15 +0200

    dsfsdfsd

--- index.js
+++ index.js
@@ -15,14 +15,33 @@ bar.onclick = function () {
   }
 };
 document.addEventListener("click", function (e) {
-  console.log(e.target)
-  console.log(e.target.classList.contains("accept"))
+  // console.log(e.target);
+  // console.log(e.target.classList.contains("accept"));
   if (e.target.classList.contains("accept")) {
-  let name =e.target.parentNode.childNodes[3]
-    let price=e.target.parentNode.childNodes[5]
-   let image= e.target.parentNode.childNodes[1]
-   let num ="+96176795291"
-   const message = `image: ${encodeURIComponent(image.src)}%0Aprice: ${price.innerHTML}%0Aname: ${name.innerHTML}`;
-   window.location.href = `https://wa.me/${num}?text=${message}`;
-}
+    let name = e.target.parentNode.childNodes[3];
+    let price = e.target.parentNode.childNodes[5];
+    let image = e.target.parentNode.childNodes[1];
+    let num = "+96176795291";
+    const message = `image: ${encodeURIComponent(image.src)}%0Aprice: ${
+      price.innerHTML
+    }%0Aname: ${name.innerHTML}`;
+    window.location.href = `https://wa.me/${num}?text=${message}`;
+  }
+});
+let sreachIcon = document.querySelector("#sreach-icon");
+let searchInput = document.querySelector(".sreach");
+let nameContents = document.querySelector(".title-content");
+// console.log(sreachIcon);
+// console.log(sreachInput);
+
+sreachIcon.addEventListener("click", function () {
+  if (this.classList.contains("resreach")) {
+    searchInput.style.cssText = `display:flex;
+    `;
+    this.classList.remove("resreach");
+  } else {
+    searchInput.style.cssText = `display:none;
+    `;
+    this.classList.add("resreach");
+  }
 });
